0eef8eb68e459fa2907c3e734d6b99d448a36dea
Billboah <wyeboah20@sahs.uhas.edu.gh>
2023-09-13 08:31:15 +0000

    commit

--- chat-app/src/App.tsx
+++ chat-app/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NotFound from './NotFound';
 import Chats from './Chat';
 import SignUp from './auth/SignUp';
 import SignIn from './auth/SignIn';
@@ -12,7 +11,6 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/" element={<Chats />} />
-        <Route element={<NotFound />} />
       </Routes>
     </Router>
   );
